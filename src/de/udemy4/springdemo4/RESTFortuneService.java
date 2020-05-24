2caf51e1aea53766f1ad553f56677be0c9314d79
Martin Langenbacher <martin.langenbacher@telekom.de>
2020-05-24 17:46:02 +0200

    finish chapter 8 ...

--- src/de/udemy4/springdemo4/RESTFortuneService.java
+++ src/de/udemy4/springdemo4/RESTFortuneService.java
@@ -2,13 +2,13 @@ package de.udemy4.springdemo4;
 
 import org.springframework.stereotype.Component;
 
-@Component
+@Component("fooMartin") // the automation of the name does not work when the second letter is also a capital letter - as the 'E' in RESTFortune...
 public class RESTFortuneService implements FortuneService {
 
 	@Override
 	public String getFortune() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return "Hier steht noch nichts drin !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
 	}
 
 }
